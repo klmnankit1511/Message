634ac7dea423c38d67a3b112c62fa625a3c0ee4d
FIRST_NAME LAST_NAMEklmnankit1511 <klmnankit1511@gmail.com>
2021-02-25 01:53:00 +0530

    Modify Insert Model

--- Backend/Model/Data.js
+++ Backend/Model/Data.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
-
-const DataSchema = new mongoose.Schema({
-  date: String,
-  type: String,
-  name: String,
-  insert: String,
-});
-
-module.exports = new mongoose.model("Data", DataSchema);
+const Datee = new mongoose.Schema({
+    _id:String,
+    count:Number,
+    data:Array
+})
+module.exports = new mongoose.model("Data",Datee);
